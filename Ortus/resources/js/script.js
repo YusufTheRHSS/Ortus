9a0f945fb4ec75c1f0dca317756ffc887cac21af
Ahmed Khan <ahmedtkhan@hotmail.com>
2020-07-24 02:57:52 -0400

    added all scripts, some may be unnecessary. changed the x to hover. made the modal pop up properly.

--- Ortus/resources/js/script.js
+++ Ortus/resources/js/script.js
@@ -1,29 +1,29 @@
-$(document).ready(function() {
 
-    // Get the modal
-    var modal = document.getElementsByClassName("bg-modal");
+//get modal
+var modalBack = document.getElementsByClassName('bg-modal')[0];
+var modalContent = document.getElementsByClassName('modal-content')[0];
+var modalBtn = document.getElementById('design-btn');
+var closeBtn = document.getElementsByClassName('close')[0];
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("design-btn");
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', clickOutside)
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+function openModal(){
+    modalBack.style.display = 'flex';
+    modalContent.style.display = 'block';
+};
 
-    // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
+function closeModal(){
+    modalBack.style.display = 'none';
+    modalContent.style.display = 'none';
+};
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
+function clickOutside(e){
+    if(e.target == modalBack){
+        modalBack.style.display = 'none';
+        modalContent.style.display = 'none';
+    };
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
+};
 
-});
